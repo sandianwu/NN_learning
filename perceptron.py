068fcdb3bb0bbf538b4de2ba34fab8c568eaa85e
sandianwu <46552515+sandianwu@users.noreply.github.com>
2019-07-21 21:24:56 +0800

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -100,6 +100,7 @@ y = np.where(y == 'Iris-setosa', -1, 1)
 X = df.iloc[0:100, [0, 2]].values
 ppn = Perceptron(eta=0.1, n_iter=10)
 ppn.fit(X, y)
+
 plot_decision_regions(X, y, classifier=ppn)
 plt.xlabel('sepal length [cm]')
 plt.ylabel('petal length [cm]')
@@ -107,7 +108,6 @@ plt.legend(loc='upper left')
 plt.show()
 
 
-#ppn.fit(X, y)
 # plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
 # plt.xlabel('Epochs')
 # plt.ylabel('Number of updates')
